1ee83ac62479d73891d02ff1d592ff7352f688b7
unknown <vijay.william@outlook.com>
2022-07-25 21:37:35 -0500

    first commit

---
+++ Practice.py
@@ -0,0 +1,18 @@
+a = input("Enter a number: \n")
+a = int(a)
+b = input("Enter another number: \n")
+b = int(b)
+list_c = ["margo","180th","184th","192nd"]
+count_c = len(list_c)
+
+def func1(a,b):
+    while a < b:
+        print(f"loop#{a}")
+        if(a < count_c):
+            print(list_c[a])
+        a = a + 1
+    print("Loop ended!!!")
+
+func1(a,b)
+
+
